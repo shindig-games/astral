c515e927928b31a3a678ca9f922889ac8b8b7d35
Shaham Noorani <snoorani@iodinesoftware.com>
2019-07-02 22:20:03 -0500

    player now animated

--- src/player/player.gd
+++ src/player/player.gd
@@ -12,16 +12,26 @@ const UP = Vector2(0, -1)
 func _physics_process(delta):
 	motion.y += GRAVITY
 	
+	if Input.is_action_pressed("ui_left"):
+		motion.x = -SPEED
+		$Sprite.flip_h = true
+		$Sprite.play("walk")
+	elif Input.is_action_pressed("ui_right"):
+		motion.x = SPEED
+		$Sprite.flip_h = false
+		$Sprite.play("walk")
+	else:
+		motion.x = 0
+		$Sprite.play("idle")
+	
 	if is_on_floor():
 		if Input.is_action_pressed("ui_up"):
 			motion.y += JUMP_HEIGHT
-	
-	if Input.is_action_pressed("ui_left"):
-		motion.x = -SPEED
-	elif Input.is_action_pressed("ui_right"):
-		motion.x = SPEED
 	else:
-		motion.x = 0
+		$Sprite.play("jump")
+	
+	if motion.y < 4:
+		$Sprite.play("fall")
 		
 		
 		
